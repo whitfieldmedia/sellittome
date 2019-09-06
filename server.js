282f1e73b57f30fe61963808e89f89b4fb876991
Spencer <backmanspencer99@gmail.com>
2019-09-06 08:47:40 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/vins', require('./routes/vins'));
 app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "/client/build/index.html"))
 })
 
 app.listen(port, () => {
