2ad758564ddd9add1c9783ec114d1b023c4391e9
spencerbackman <backmanspencer99@gmail.com>
2019-10-16 13:13:23 -0500

    added process.env.PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require("dotenv").config();
 const path = require("path");
-const port = 5800;
+const port = process.env.PORT || 5800;
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
