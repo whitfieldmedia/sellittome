041dcc24c63c3ad7776c5c388712b6217a2bea72
spencerbackman <backmanspencer99@gmail.com>
2019-10-31 15:29:16 -0500

    ran build script

---
+++ client/build/precache-manifest.03682358654f8526332a31e300f994b4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d0f2199ec51f3d7235993b7f6f915f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1847631530fc50957743",
+    "url": "/static/css/main.eb0bf255.chunk.css"
+  },
+  {
+    "revision": "85c552051858bd1dd336",
+    "url": "/static/js/2.1b939dbd.chunk.js"
+  },
+  {
+    "revision": "1847631530fc50957743",
+    "url": "/static/js/main.0a6d6aac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "74d71190bd47266a21e1fafc5217290c",
+    "url": "/static/media/automobile.74d71190.svg"
+  },
+  {
+    "revision": "ef6b2e0b17ed7c8e1c7cce71f27abeb5",
+    "url": "/static/media/big-bubble.ef6b2e0b.png"
+  },
+  {
+    "revision": "9531a6ed5825fc176c383ce7109e5ba0",
+    "url": "/static/media/dots.9531a6ed.svg"
+  },
+  {
+    "revision": "cbc94b017e8fedf94e66048f65a04072",
+    "url": "/static/media/header-bubble.cbc94b01.png"
+  },
+  {
+    "revision": "0fe614b582cf53f1a704b0fe316887af",
+    "url": "/static/media/headerBig.0fe614b5.png"
+  },
+  {
+    "revision": "c17d189d0d719d316da4771104b90b4e",
+    "url": "/static/media/headerMid.c17d189d.png"
+  },
+  {
+    "revision": "6d377334959d47740e2727cc40e3f98b",
+    "url": "/static/media/money-bag.6d377334.svg"
+  },
+  {
+    "revision": "37e4dacda31c4ffcd5da8c58d2682f43",
+    "url": "/static/media/mr-cash-flying.37e4dacd.png"
+  },
+  {
+    "revision": "092b08626c3375b45907b59d69aeb89c",
+    "url": "/static/media/mrcashtall2.092b0862.png"
+  },
+  {
+    "revision": "4c9f098dae66eccdfbc358fd2d8700be",
+    "url": "/static/media/offer-button.4c9f098d.svg"
+  },
+  {
+    "revision": "492d1426ec08554e9e5c85f73714d166",
+    "url": "/static/media/offer.492d1426.svg"
+  }
+]);
